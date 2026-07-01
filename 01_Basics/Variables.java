c96611827f82469f1bca5520232f7836457bd930
Varnapriya <varnapriya2659@gmail.com>
2026-07-01 22:39:30 +0530

    Add student profile using variables
    Demonstrated the use of different Java variable types including String, int, double, char, and boolean through a simple student profile program.

---
+++ 01_Basics/Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        String name = "Varna Priya";
+        int age = 21;
+        double cgpa = 8.7;
+        char grade = 'A';
+       
+
+        System.out.println("----- Student Profile -----");
+        System.out.println("Name    : " + name);
+        System.out.println("Age     : " + age);
+        System.out.println("CGPA    : " + cgpa);
+        System.out.println("Grade   : " + grade);
+       
+    }
+}
